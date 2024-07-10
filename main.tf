51d654bf45cee5064fb1190b81bcd946d8f90013
Kbpandit <166830669+Kbpandit@users.noreply.github.com>
2024-07-10 18:26:38 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "ap-south-1"
+
+}
+
+resource "aws_instance" "kblinux" {
+    ami = var.ami_value
+    instance_type = var.instance_type_value
+    key_name = var.key_name_value
+  
+}
